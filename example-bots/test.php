09b147a045f3c6bee4fe0357b212630c84df79b2
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-24 15:03:41 +0200

    Update test.php

--- example-bots/test.php
+++ example-bots/test.php
@@ -24,7 +24,9 @@ $button		= $bot->SingleInlineUrlKeyboard("Engineering support group 👩‍💻"
 $users		= file_get_contents("users.txt");
 $_users		= explode("\n", $users);
 if($text == "/co"){
-$bot->sendMessage($chat_id, $message = 'yes i know', "HTML", true, $message_id, $button);
+$m = $bot->sendMessage($chat_id, $message = 'yes i know', "HTML", true, $message_id, $button)['result']['message_id'];
+sleep(3);
+$bot->editMessage($chat_id, $message_id = $m, $text = "edit", $mode = null, $webPage = null, $button = null);
 sleep(3);
 $bot->deleteMessage($chat_id, $message_id);}
 
