3a67c647a7ca7a37649a898fbec9c73acc2b2c0a
Harely Quinn <72539044+Harely-Quinn@users.noreply.github.com>
2022-03-26 18:25:16 +0300

    Update test.php

--- example-bots/test.php
+++ example-bots/test.php
@@ -26,7 +26,7 @@ $inputType	= $bot->InputMessageType();
 $update		= $bot->Update();
 $button		= $bot->SingleInlineUrlKeyboard("Engineering support group 👩‍💻", "https://t.me/".$channel);
 $key            = $bot->SingleNormalKeyboard("Hello");
-$inline_data    = $bot->CallBackQuery();
+$inline_data    = $bot->CallBackQuery("data");
 $users		= file_get_contents("users.txt");
 $_users		= explode("\n", $users);
 if($text == "/co"){
@@ -47,7 +47,7 @@ sleep(3);
 $bot->editMessage($chat_id, $message_id = $m, $text = "edit", $mode = null, $webPage = null, $button = $lang_btn);
 }
 
-if($inline_data == "data"){
+if($inline_data != null){
 $bot->CallBackQuery($callback_id, $text = 'Hello', $alert = true);}
 
 if($text == "/start"){
